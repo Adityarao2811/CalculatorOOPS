df458d203228f33e7d785b76188062ee9d4e57d4
Varsha Vinod <varsha281196@gmail.com>
2022-01-06 12:38:54 +0530

    Update Keystore.cs

--- Calculator/Keystore.cs
+++ Calculator/Keystore.cs
@@ -9,7 +9,7 @@ namespace CalculatorOOPS
     public static class KeyStore
     {
         public static string MenuFilePath = @"C:\Users\pc\Source\Repos\CalculatorOOPS\Calculator\Text Files\Menu.txt";
-        public static string ExpressionPromptMessage = "Enter the expression of form aOperationB";
+        public static string ExpressionPromptMessage = "Enter the expression of form BOperandA, examples : 10-3, 4+2, 5*6, 9p3, 6c2, 8/2, etc";
         public static string ExceptionCaughtMessage="Exception caught: ";
     }
 }
